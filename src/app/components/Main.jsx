69d4868accbc765a7d45f5a04f0da943705bbb96
Rohit-Web-Dev-2001 <rohit1447481@gmail.com>
2025-07-16 12:15:10 +0530

    Bug Fix

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,9 +2,9 @@
 import { React, useContext, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./NavBar/Navabar";
-import HomPage from "./Hompage/HomPage";
+import HomPage from "./Hompage/Hompage";
 import AboutUs from "./AboutUS/AboutUs";
-import ProjectList from "./ProjectList/ProjectList";
+import ProjectList from "./ProjectList/Projectlist";
 import ContactMe from"./ContactUS/ContactMe";
 
 
